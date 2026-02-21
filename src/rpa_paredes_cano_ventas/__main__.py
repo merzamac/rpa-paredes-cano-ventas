85575cf8de2cabef8a82989531204e3170d4016b
merzamac <100945727+merzamac@users.noreply.github.com>
2026-02-20 22:10:47 -0400

    fix:main

--- src/rpa_paredes_cano_ventas/__main__.py
+++ src/rpa_paredes_cano_ventas/__main__.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass
 from asyncio.log import logger
+from rpa_paredes_cano_ventas import routes
+from rpa_paredes_cano_ventas.types import DAtaCSV
+from rpa_paredes_cano_ventas.models.header import Header
+from rpa_paredes_cano_ventas.models.processable import ProcessableFile
+from rpa_paredes_cano_ventas.readers.fastexcel_reader import ExcelPLEReader
 from rpa_paredes_cano_ventas.orchestrator.file_processor import FileProcessor
 from rpa_paredes_cano_ventas.orchestrator.import_platform import ImportPlatform
 from rpa_paredes_cano_ventas.orchestrator.aconsys_platform import AconsysPlatform
+from rpa_paredes_cano_ventas.orchestrator.business_rules import BusinessRulesWithApps
 from rpa_paredes_cano_ventas.readers.read_input_output import ReadOutputCSVPreviousMonth
-from rpa_paredes_cano_ventas import routes
-from rpa_paredes_cano_ventas.types import DAtaCSV
-from rpa_paredes_cano_ventas.models.processable import ProcessableFile
-from rpa_paredes_cano_ventas.readers.fastexcel_reader import ExcelPLEReader
-from rpa_paredes_cano_ventas.models.header import Header
 
 
 class BotOrchestrator:
@@ -44,29 +44,9 @@ class BotOrchestrator:
                 )
 
                 BusinessRulesWithApps.execute(csv_outputs)
+                logger.info("Proceso finalizado con éxito.")
         BusinessRulesWithApps.execute(csv_outputs)
-        print("Proceso de registro de series completado.")
-
-
-@dataclass(frozen=True, slots=True)
-class BusinessRulesWithApps:
-    @staticmethod
-    def execute(csv_outputs):
-        # 2. Carga y Verificación
-        import_results = import_app.upload_csvs(csv_outputs)
-
-        if import_results.has_no_details():
-            print("Proceso finalizado con éxito.")
-            return
-
-        # 3. Gestión de Diferencias (Aconsys)
-        aconsys_files = aconsys_app.download_reports()
-        new_series = file_service.identify_new_series(import_results, aconsys_files)
-
-        if new_series:
-            # 4. Registro en ambas plataformas
-            aconsys_app.register_series(new_series)
-            import_app.register_series(new_series)
+        logger.info("Proceso finalizado con éxito.")
 
 
 # --- Ejecución ---
