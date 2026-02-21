2b1dc9c916fd01553c1720cdab66b0138ffaf5b1
merzamac <100945727+merzamac@users.noreply.github.com>
2026-02-20 22:11:52 -0400

    add:business rule

---
+++ src/rpa_paredes_cano_ventas/orchestrator/business_rules.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, slots=True)
+class BusinessRulesWithApps:
+    @staticmethod
+    def execute(csv_outputs):
+        # 2. Carga y Verificación
+        import_results = import_app.upload_csvs(csv_outputs)
+
+        if import_results.has_no_details():
+            print("Proceso finalizado con éxito.")
+            return
+
+        # 3. Gestión de Diferencias (Aconsys)
+        aconsys_files = aconsys_app.download_reports()
+        new_series = file_service.identify_new_series(import_results, aconsys_files)
+
+        if new_series:
+            # 4. Registro en ambas plataformas
+            aconsys_app.register_series(new_series)
+            import_app.register_series(new_series)
